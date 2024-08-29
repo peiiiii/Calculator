753a8230b5b854446c239c745bd2e524cacc6452
pei <pei860318@gmail.com>
2024-08-29 11:07:19 -0700

    change return type from CustomizedResponse to ResponseEntity

--- src/main/java/com/ebay/calculator/controller/CalculatorController.java
+++ src/main/java/com/ebay/calculator/controller/CalculatorController.java
@@ -3,7 +3,8 @@ package com.ebay.calculator.controller;
 import java.math.BigDecimal;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,15 +27,15 @@ public class CalculatorController {
     }
 
     @PostMapping("/calculate")
-	public CustomizedResponse calculate(@RequestBody CalculateRequest calculateRequest) {
+	public ResponseEntity<CustomizedResponse> calculate(@RequestBody CalculateRequest calculateRequest) {
 		BigDecimal result = calculatorService.calculateOperation(calculateRequest.getOperation(), calculateRequest.getNum1(), calculateRequest.getNum2());
-		System.out.println("result : " + result);
-		return CustomizedResponse.builder().code("200").message("Calculate operation successful").data(result).success(true).build();
+		// return new ResponseEntity<>(result, HttpStatus.OK);
+		return new ResponseEntity<>(CustomizedResponse.builder().code("200").message("Calculate operation successful").data(result).success(true).build(), HttpStatus.OK);
 	}
 
 	@PostMapping("/chain")
-	public CustomizedResponse chainOperations(@RequestBody ChainRequest chainRequest) {
+	public ResponseEntity<CustomizedResponse> chainOperations(@RequestBody ChainRequest chainRequest) {
 		BigDecimal result = calculatorService.chainOperations(chainRequest.getOperations(), chainRequest.getInitialValue(), chainRequest.getValues());
-		return CustomizedResponse.builder().code("200").message("Chaining operations successful").data(result).success(true).build();
+		return new ResponseEntity<>(CustomizedResponse.builder().code("200").message("Chaining operations successful").data(result).success(true).build(), HttpStatus.OK);
 	}
 }
