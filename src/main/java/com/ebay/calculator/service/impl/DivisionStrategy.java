d3b4d38e5db37c3dd14cb50f95836b5a998f0815
pei <pei860318@gmail.com>
2024-08-29 04:56:39 -0700

    add tests

--- src/main/java/com/ebay/calculator/service/impl/DivisionStrategy.java
+++ src/main/java/com/ebay/calculator/service/impl/DivisionStrategy.java
@@ -1,16 +1,18 @@
 package com.ebay.calculator.service.impl;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 import com.ebay.calculator.exception.CalculationException;
 import com.ebay.calculator.service.OperationStrategy;
 
 public class DivisionStrategy implements OperationStrategy {
+    private final int SCALE = 3;
     @Override
     public BigDecimal apply(BigDecimal num1, BigDecimal num2) {
         if (num2.compareTo(BigDecimal.ZERO) == 0) {
             throw new CalculationException("Division by zero is not allowed", "400");
         }
-        return num1.divide(num2);
+        return num1.divide(num2, SCALE, RoundingMode.HALF_UP);
     }
 }
---
+++ src/test/java/com/ebay/calculator/CalculatorTest.java
@@ -0,0 +1,101 @@
+package com.ebay.calculator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
+import com.ebay.calculator.exception.CalculationException;
+import com.ebay.calculator.service.impl.Calculator;
+
+public class CalculatorTest {
+    @InjectMocks
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        Number num1 = 2, number2 = 7;
+    }
+
+    @Test
+    void testCalculateAddition() {
+        BigDecimal result = calculator.calculateOperation(Optional.of("add"), Optional.of(5), Optional.of(3));
+        assertEquals(BigDecimal.valueOf(8.0), result);
+    }
+
+    @Test
+    void testCalculateSubtraction() {
+        BigDecimal result = calculator.calculateOperation(Optional.of("subtract"), Optional.of(5), Optional.of(3));
+        assertEquals(BigDecimal.valueOf(2.0), result);
+    }
+
+    @Test
+    void testCalculateMultiplication() {
+        BigDecimal result = calculator.calculateOperation(Optional.of("MULTIPLY"), Optional.of(5), Optional.of(3));
+        assertEquals(BigDecimal.valueOf(15.0), result.setScale(1, RoundingMode.HALF_UP));
+    }
+
+    @Test
+    void testCalculateDivision() {
+        BigDecimal result = calculator.calculateOperation(Optional.of("Divide"), Optional.of(5), Optional.of(3));
+        assertEquals(BigDecimal.valueOf(1.667), result);
+    }
+
+    @Test
+    void testCalculateDivisionByZero() {
+        Exception exception = assertThrows(CalculationException.class, () -> {
+            calculator.calculateOperation(Optional.of("Divide"), Optional.of(5), Optional.of(0));
+        });
+        assertEquals("Division by zero is not allowed", exception.getMessage());
+    }
+
+    @Test
+    void testCalculateInvalidOperation() {
+        Exception exception = assertThrows(CalculationException.class, () -> {
+            calculator.calculateOperation(null, Optional.of(5), Optional.of(3));
+        });
+        assertEquals("Operation cannot be null or empty", exception.getMessage());
+    }
+
+    @Test
+    void testCalculateInvalidNumber() {
+        Exception exception = assertThrows(CalculationException.class, () -> {
+            calculator.calculateOperation(Optional.of("add"), Optional.empty(), Optional.of(3));
+        });
+        assertEquals("Numbers cannot be null", exception.getMessage());
+    }
+
+    @Test
+    void testChainOperations() {
+        BigDecimal result = calculator.chainOperations(
+            Optional.of(new String[]{"add", "Multiply"}),
+            Optional.of(5), 
+            Optional.of(new Number[]{3,2}));
+        BigDecimal bd = new BigDecimal("16.00");
+        assertTrue(bd.equals(result));
+    }
+
+    @Test
+    void testChainOperationsWithOperationsWithMismatchedLengths() {
+        // when(calculatorService.calculateOperation(any(Number.class), any(Operation.class), any(Number.class)))
+        //     .thenThrow(new CalculationException("Operation not supported", "INVALID_OP"));
+
+        Exception exception = assertThrows(CalculationException.class, () -> {
+            calculator.chainOperations(
+                Optional.of(new String[]{"add", "add", "add"}),
+                Optional.of(5), 
+                Optional.of(new Number[]{3,2}));
+        });
+        assertEquals("Number of operations must match number of values", exception.getMessage());
+    }
+
+}
